4cda90effbee541e0768bd397e8b939d9bcb180a
napol <n.suksaran@gmail.com>
2021-03-25 15:21:31 +0700

    แก้

--- src/views/Employee/GuidePoint.js
+++ src/views/Employee/GuidePoint.js
@@ -61,33 +61,6 @@ class GuidePoint extends React.Component {
             </h3>
           </CModalBody>
         </CModal>
-        {/* <Modal
-            title="หมายเหตุคะแนน"
-            visible={this.state.visible}
-            onOk={this.handleOk}
-            onCancel={this.handleCancel}
-            okButtonProps={{ disabled: true }}
-            cancelButtonProps={{ disabled: true }}
-            footer={null}
-            width="35%"
-            closeIcon={<AiFillCloseCircle color="#F04D22" size="50%"/>}
-        >
-            <Row>
-                <Col>
-                    <img className="guidepointpic" src="/logo/หมายเหตุคะแนน.png" alt="หมายเหตุคะแนน" />
-                </Col>
-                <Col >
-                    <p>4 = ประสิทธิภาพดีเยี่ยม ควรเป็นแบบอย่าง</p>
-                    <p>3.5 = ประสิทธิภาพดีมาก</p>
-                    <p>3 = ประสิทธิภาพดี</p>
-                    <p>2.5 = ประสิทธิภาพพอใช้ ต้องปรับปรุง</p>
-                    <p>2 = ต้องการปรับปรุงพัฒนา</p>
-                    <p>1.5 = ต้องการปรับปรุงพัฒนาอย่างมาก</p>
-                    <p>1 = ต้องการปรับปรุงพัฒนาอย่างเร่งด่วน</p>
-                </Col>
-            </Row>
-            <h3 style={{ textAlign: "center", marginTop: "2%" }}>**คะแนนในส่วนนี้คิดเป็นคะแนนเต็ม 70 คะแนน ของผลงานในภาพรวมทั้งหมด</h3>
-        </Modal> */}
       </>
     );
   }
